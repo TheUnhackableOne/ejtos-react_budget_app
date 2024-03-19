2720d4cb656dbfa45ca3690494861774273328ff
Niranjan Nishan <124856381+TheUnhackableOne@users.noreply.github.com>
2024-03-19 11:25:42 +0530

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,31 +1,18 @@
-import React, { useContext } from "react";
-import { AppContext } from "../context/AppContext";
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
-const Remaining = () => {
-  const { expenses, budget, Currency } = useContext(AppContext);
+const RemaningValue = () => {
+    const { expenses, Currency, budget } = useContext(AppContext);
+    const remaining = budget - (expenses.reduce((total, item) => {
+        return (total += (item.quantity));
+    }, 0));
 
-  const totalExpenses = expenses.reduce((total, item) => {
-    return (total += item.cost);
-  }, 0);
-
-  const remaining = budget - totalExpenses;
-
-  const alertType = remaining < 0 ? "alert-danger" : "alert-success";
-
-  const handleAlert = () => {
-    if (remaining < 0) {
-      alert("You have exceeded your budget!");
-    }
-  };
-
-  return (
-    <div className={`alert ${alertType}`} onClick={handleAlert}>
-      <span>
-        Remaining: {Currency} {remaining}
-      </span>
-    </div>
-  );
+    return (
+        <div className='alert alert-primary'>
+            <span>Remaining: {Currency}{remaining}</span>
+        </div>
+    );
 };
 
-export default Remaining;
+export default RemaningValue;
 
