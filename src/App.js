9faafd4e713740a58bb97b5b52ce9615cd504aa5
Niranjan Nishan <124856381+TheUnhackableOne@users.noreply.github.com>
2024-03-19 11:17:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,78 +1,48 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-//Code to import Budget.js
-import Budget from './components/Budget';
-
-// Add code to import the other components here under
-import Remaining from './components/Remaining';
-import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseList from './components/ExpenseList';
-import AllocationForm from './components/AllocationForm';
-import Currency from "./components/Currency";
-
-
-
 
 
 import { AppProvider } from './context/AppContext';
+import BudgetValue from './components/BudgetValue';
+import RemainingValue from './components/RemainingValue';
+import TotalAllocatedValue from './components/TotalAllocatedValue';
+import DepartmentList from './components/DepartmentList';
+import DepartmentSelected from './components/DepartmentSelected';
+import Currency from './components/Currency';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                                         // Budget component
-                                 <div className='col-sm'>
-                                 <Budget />
-                               </div>   /* Add Budget component here */
-                        }        
-
-                        {
-                                                               //Remaining component
+                <div className='row mt-3'> 
                     <div className='col-sm'>
-                    <Remaining />
+                        <BudgetValue />
+                    </div>
+                    <div className='col-sm'>
+                        <RemainingValue />
+                    </div>
+                    <div className='col-sm'>
+                        <TotalAllocatedValue />
+                    </div>
+                    <div className='col-sm'>
+                        <Currency />
+                    </div>
                 </div>
-                                              
-                                              /* Add Remaining component here*/
-                        }        
-
-                        {
-                                            //ExpenseTotal component
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
                     <div className='col-sm'>
-                    <ExpenseTotal />
-                </div> /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                           <div className='col-sm'>
-                           <ExpenseList/>
-                        </div> /* Add ExpenseList component here */
-                        }         
-
-                        {
-                                <div className='col-sm'>
-                                <ExpenseList />
-                            </div>/* Add ExpenseItem component here */
-                        }        
-
-                        {
-                           <div className='col-sm'>
-                           <AllocationForm/>
-                       </div> /* Add AllocationForm component here under */
-                        } 
-                        {
-                           <div className='col-sm'>
-                           <Currency/>
-                       </div> 
-                        }
-                       
-                               
+                        <DepartmentList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Change Allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <DepartmentSelected/>
+                    </div>
                 </div>
             </div>
         </AppProvider>
-    );
-};
+    ); // return
+}; // App
 export default App;
